0a8aed5fb091e2eb5feb4f361762c17cf3cf4cca
James Butler <stupidscript@gmail.com>
2019-10-22 19:56:26 -0700

    tweaking frontpage positioning

--- frontpage.php
+++ frontpage.php
@@ -70,14 +70,14 @@ if (!empty($_GET['admin'])) {
 ?>
 <?=$checkin?><a href="ARES_Comms_Log.php<?=$isadmin?>" target="_blank" class="colhead"><big>ARES Net Control Comms Log</big></a><br><br>
 <!--/p-->
-<p style="border-top:solid 1px #900;margin-top:0px;margin-bottom:0px;padding-top:20px;">
+<div style="border-top:solid 1px #900;margin:0;padding-top:20px;">
 <?php
 #}
 ?>
 <a href="https://www.freelists.org/list/arestoolkit" target="_blank">ARES Toolkit Mailing List</a> | <a href="forum/index.php" target="_blank">ARES Toolkit Forum</a> &nbsp;|&nbsp; <a href="javascript:popIRC()" class="colhead">ARESLAX IRC Chat</a>
 <div style="border-top:solid 1px #900;margin:20px 0 0 0;padding-top:20px;">
-<div onclick="javascript:divVis('forms')" style="cursor:pointer;font-weight:bold;margin:0 0 6px 0;padding:4px;border:solid 1px lightgrey;border-radius:4px;background-color:rgb(255,255,230);">FORMS: Download or Fill &amp; Print:</div>
-<div id="forms" style="display:none;margin:0">
+<div onclick="javascript:divVis('forms')" style="cursor:pointer;font-weight:bold;margin:0 0 20px 0;padding:4px;border:solid 1px lightgrey;border-radius:4px;background-color:rgb(255,255,230);">FORMS: Download or Fill &amp; Print:</div>
+<div id="forms" style="display:none;margin:0 0 16px 0;">
 <table border=0 cellpadding=6 cellspacing=0>
 <tr valign=top><td align=center>
 <a href="files/20190703_Amateur_Radio_MCI_Poll_Form_2011_form.pdf" class="colhead">ARES MCI Poll</a><br>
@@ -95,7 +95,7 @@ if (!empty($_GET['admin'])) {
 </div>
 </div>
 
-<br></td></tr>
+</td></tr>
 </table>
 
 <table border=0 cellpadding=6 cellspacing=0 style="margin-top:12px">
