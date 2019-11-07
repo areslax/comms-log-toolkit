8fb6cb834f224f1c98ee654c691d8c2fe57f017f
James Butler <stupidscript@gmail.com>
2019-11-07 12:41:49 -0800

    modifying op status to use gps for loc and nc

--- ARES_INCLUDE_Operator_Status.php
+++ ARES_INCLUDE_Operator_Status.php
@@ -6,35 +6,41 @@
  * ###################################### */
 ?>
 <!-- ARES_INCLUDE_Operator_Status.php -->
-<div style='position:fixed;top:10px;left:10px;padding:0px;background-color:white;text-align:center;'>
-<!--div id=members style='position:fixed;top:10px;left:10px;padding:0px;background-color:white;text-align:center;'-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap" rel="stylesheet">
+<div id=opstable style='position:fixed;top:10px;left:10px;padding:0px;background-color:white;text-align:center;'>
 <div id=members>
 <table id=table_memstatus border=1 cellpadding=1 cellspacing=0 style='border-color:white;width:280px;'>
-<tr><th colspan=4>Operator Status</th></tr>
+<tr><th colspan=3>Operator Status</th><th style="font-family:'Open Sans Condensed',sans-serif;font-size:.8em;">LOC</th><th style="font-family:'Open Sans Condensed',sans-serif;font-size:.8em;">NC</th></tr>
 <?php
 $ops = array();
 if (!empty($iid)) {
 	//single incident
-	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,i_id,l_id,nc_id,s_title from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where i_id=:iid");
+	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,Operators.i_id,i_name,Operators.l_id,l_tactical,l_name,Operators.nc_id,nc_callsign,s_title from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Locations on Locations.l_id=Members.m_prestage_lid left outer join Net_Controls on Net_Controls.nc_id=Operators.nc_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where i_id=:iid");
 	$oq->execute(array(':iid'=>$iid));
 	$labl = "Involved in this Incident";
 }
 else if (!empty($lid)) {
 	//single location
-        $oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,i_id,l_id,nc_id,s_title from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where l_id=:lid");
-        $oq->execute(array(':lid'=>$lid));
+	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,m_prestage_lid,Operators.i_id,i_name,Operators.l_id,l_tactical,l_name,Operators.nc_id,s_title,s_data,s_color from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Incidents on Incidents.i_id=Operators.i_id left outer join Locations on Locations.l_id=Operators.l_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where Locations.l_id=:lid");
+	$oq->execute(array(':lid'=>$lid));
 	$labl = "Deploys to this Location";
 }
 else if (!empty($ncid)) {
 	//single net control
-        $oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,i_id,l_id,nc_id,s_title from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where nc_id=:ncid");
-        $oq->execute(array(':ncid'=>$ncid));
+	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,m_prestage_lid,Operators.i_id,i_name,Operators.l_id,l_tactical,l_name,Operators.nc_id,s_title,s_data,s_color from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Incidents on Incidents.i_id=Operators.i_id left outer join Locations on Locations.l_id=Operators.l_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status where Net_Controls.nc_id=:ncid");
+	$oq->execute(array(':ncid'=>$ncid));
 	$labl = "Associated with this Net Control";
 }
 else {
 	//all operators
-	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,m_prestage_lid,Operators.i_id,Operators.l_id,Operators.nc_id,s_title,s_data,s_color,l_tactical,l_name,nc_callsign from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status left outer join Locations on Locations.l_id=Operators.l_id left outer join Net_Controls on Net_Controls.nc_id=Operators.nc_id");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$oq = $conn->prepare("select Members.m_id,m_callsign,m_status,m_fname,m_lname,m_prestage_lid,Operators.i_id,i_name,Operators.l_id,l_tactical,l_name,Operators.nc_id,s_title,s_data,s_color from Members left outer join Operators on Operators.m_id=Members.m_id left outer join Incidents on Incidents.i_id=Operators.i_id left outer join Locations on Locations.l_id=Operators.l_id left outer join Status_Codes on Status_Codes.s_id=Members.m_status");
+try{
 	$oq->execute();
+}catch(PDOException $e){
+	print($e->getMessage());
+	exit;
+}
 	$labl = "";
 }
 $orow = $oq->fetchAll(PDO::FETCH_ASSOC);
@@ -45,17 +51,21 @@ foreach($orow as $o) {
 	$ops[$cs]['incident'] = $o['i_id'];
 	$ops[$cs]['location'] = (!empty($o['l_id'])) ? $o['l_id']:$o['m_prestage_lid'];
 	if (empty($o['l_id'])) {
-		$lq = $conn->prepare("select l_tactical,l_name from Locations where l_id=:pid limit 1");
+		$lq = $conn->prepare("select l_tactical,l_name,l_gps from Locations where l_id=:pid limit 1");
 		$lq->execute(array(':pid'=>$o['m_prestage_lid']));
 		$larr = $lq->fetchAll(PDO::FETCH_ASSOC);
 		foreach($larr as $l) {
 			$ops[$cs]['locationname'] = $l['l_tactical'].": ".$l['l_name'];
+			$ops[$cs]['locationgps'] = $l['l_gps'];
 		}
 	}
 	else {
 		$ops[$cs]['locationname'] = $o['l_tactical'].": ".$o['l_name'];
+		$ops[$cs]['locationgps'] = $o['l_gps'];
 	}
 	$ops[$cs]['netcontrol'] = $o['nc_id'];
+	$ops[$cs]['netcontrolname'] = $o['nc_callsign'];
+	$ops[$cs]['netcontrolgps'] = $o['nc_gps'];
 	$ops[$cs]['statusid'] = $o['m_status'];
 	$ops[$cs]['status'] = $o['s_title'];
 	$ops[$cs]['statusclr'] = $o['s_color'];
@@ -65,7 +75,7 @@ foreach($orow as $o) {
 }
 ksort($ops);
 foreach($ops as $cs => $data) {
-	echo "<tr><td align=left title='".$data['locationname']."' style='cursor:pointer'>".$cs."</td><td align=left><a href='ARES_Member_Manage.php?mid=".$data['mid']."&admin=".$_GET['admin']."' target='_blank' title='Click to view ".$cs." Member Details'>".$data['name']."</a></td><td style='background-color:".$data['statusclr'].";cursor:pointer;' title='".$data['statustxt']."' onclick=\"showStatusEdit(".$data['mid'].",'".$cs.": ".$data['name']."',".$data['statusid'].")\">".$data['status']."</td><th cellpadding=0><input type=checkbox title='".$labl."'></th></tr>\n";
+	echo "<tr><td align=left title='".$data['locationname']."' style='cursor:pointer'>".$cs."</td><td align=left><a href='ARES_Member_Manage.php?mid=".$data['mid']."&admin=".$_GET['admin']."' target='_blank' title='Click to view ".$cs." Member Details'>".$data['name']."</a></td><td style='background-color:".$data['statusclr'].";cursor:pointer;' title='".$data['statustxt']."' onclick=\"showStatusEdit(".$data['mid'].",'".$cs.": ".$data['name']."',".$data['statusid'].")\">".$data['status']."</td><th cellpadding=0><a href='https://maps.google.com/?q=".$data['locationgps']."' target='_blank' title='".$data['locationname']."'><img src='images/icon-google-maps.svg' width=14 border=0 style='opacity:.3'></a></th><th cellpadding=0><a href='https://maps.google.com/?q=".$data['netcontrolgps']."' target='_blank' title='".$data['netcontrolcall']."'><img src='images/icon-google-maps.svg' width=14 border=0i style='opacity:.3'></a></th></tr>\n";
 }
 ?>
 </table>
